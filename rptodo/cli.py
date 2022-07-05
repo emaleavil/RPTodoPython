51f04fee8b54d7f098b428857894d8dd21d8d7d2
Emanuel Leal <enmanuel.leal@sngular.com>
2022-07-05 08:31:31 +0200

    Implement init command

--- rptodo/cli.py
+++ rptodo/cli.py
@@ -1,14 +1,44 @@
 """ This module provides the RP To-Do CLI."""
 # rptodo/cli.py
 
+from pathlib import Path
 from typing import Optional
 
 import typer
 
-from rptodo import __version__, __app_name__
+from rptodo import __version__, __app_name__, ERRORS, config, database
 
 app = typer.Typer()
 
+@app.command()
+def init(
+    db_path: str = typer.Option(
+        str(database.DEFAULT_DB_FILE_PATH), # Default database path
+        '--db-path',
+        '-db',
+        prompt = 'to-do database location?'
+    )
+) -> None:
+    """Initialie the to-do database."""
+    app_init_error = config.init_app(db_path)
+    if app_init_error:
+        typer.secho(
+            f'Creating database failed with "{ERRORS[app_init_error]}"',
+            fg = typer.colors.RED
+        )
+        raise typer.Exit(1)
+    db_init_error = database.init_database(Path(db_path))
+    if db_init_error:
+        typer.secho(
+            f'Creating database failed with "{ERRORS[db_init_error]}"',
+            fg = typer.colors.RED,
+        )
+        raise typer.Exit(1)
+    else:
+        typer.secho(
+            f'The to-do database is {db_path}',
+            fg = typer.colors.GREEN
+        )
 
 def version__callback(value: bool) -> None:
     """Method that prints version when --version or -v option are requested."""
@@ -28,4 +58,5 @@ def main(
         is_eager=True
     )
 ) -> None:
+    """Method that executes version callback to print app version."""
     return
