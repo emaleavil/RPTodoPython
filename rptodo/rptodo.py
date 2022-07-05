5f025deaf8323dfe4eba50a87bdfcc1fc3403c97
Emanuel Leal <enmanuel.leal@sngular.com>
2022-07-05 11:59:57 +0200

    Add controller class

--- rptodo/rptodo.py
+++ rptodo/rptodo.py
@@ -1,9 +1,18 @@
 """This module provides the RP To-Do model-controller."""
 # rptodo/rptodo.py
 
+from pathlib import Path
 from typing import Any, Dict, NamedTuple
+
+from rptodo.database import DatabaseHandler
 
 class CurrentTodo(NamedTuple):
     """This class define the current to-do object."""
     todo: Dict[str, Any]
     error: int
+    
+    
+class Todoer:
+    """Controller that connects application logic with database."""
+    def __init__(self, db_path: Path) -> None:
+        self.db_handler = DatabaseHandler(db_path)
